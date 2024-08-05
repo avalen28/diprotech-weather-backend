86a3ee8641e5b4c58bfbcb6e9edc2de492783631
Alberto Valenzuela <avalen28@gmail.com>
2024-08-05 14:59:33 +0200

    B01: Removes unused files/folders

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+
+const router = require("express").Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({message: "hello world!"});
 });
 
 module.exports = router;
