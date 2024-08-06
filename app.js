3d4dfceaef16e02db8681a810d5c7bf56c7b9ca6
Alberto Valenzuela <avalen28@gmail.com>
2024-08-06 11:02:25 +0200

    Adds Cors

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const weatherDataRouter = require("./routes/weatherData");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+app.set("trust proxy", 1);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
