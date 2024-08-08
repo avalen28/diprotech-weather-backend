de27c48a7eeed8e5efffb7941646e213dc993093
Alberto Valenzuela <avalen28@gmail.com>
2024-08-08 12:00:28 +0200

    Upgrade storeData. Now onle store one weather-data by day and city

--- utils/storage.js
+++ utils/storage.js
@@ -4,16 +4,19 @@ const fs = require("fs");
 /**
  * Stores weather data to a JSON file.
  * @param {Object} dataToStore - The weather data to store.
- * @param {string} dataToStore.city - The name of the city for which the weather data is being stored.
+ * @param {string} dataToStore.city.name - The name of the city for which the weather data is being stored.
  */
 const storeData = (dataToStore) => {
   const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const day = String(today.getDate()).padStart(2, "0");
+  const formattedDate = `${year}-${month}-${day}`;
+
   const parentDir = path.dirname(__dirname);
   const historicWeatherDataPath = path.join(parentDir, "historic-weather-data");
-  const filePath = path.join(
-    historicWeatherDataPath,
-    `${today}_${dataToStore.city}.json`
-  );
+  const fileName = `${formattedDate}_${dataToStore.city.name}.json`;
+  const filePath = path.join(historicWeatherDataPath, fileName);
 
   if (fs.existsSync(filePath)) {
     return;
